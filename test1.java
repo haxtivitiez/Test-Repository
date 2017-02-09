9a9b3f5a2d928a9ed08e92c8003b02e8d2da5379
Un4ckn0wl3z <gh0stsync.rt@gmail.com>
2017-02-09 14:45:36 +0700

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,12 @@
+public class  Randomex{
+	public static void main(String[] args) {
+
+		
+			for(int i=0;i<10;i++){
+					double r = Math.random() * 10;
+					int rand = (int) r;
+					System.out.println(rand);
+			}
+	
+	}
+}
