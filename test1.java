3e4df6c98fe029b256c6aa89df34325b93ef4019
Un4ckn0wl3z <gh0stsync.rt@gmail.com>
2017-02-09 14:48:04 +0700

    Update test1.java

--- test1.java
+++ test1.java
@@ -2,10 +2,11 @@ public class  Randomex{
 	public static void main(String[] args) {
 
 		
-			for(int i=0;i<10;i++){
+			for(int f=0;f<10;f++){
 					double r = Math.random() * 10;
 					int rand = (int) r;
 					System.out.println(rand);
+				// test edited
 			}
 	
 	}
