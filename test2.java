edc0b0f3e461a1b51931c35d4f18d6a84c33efeb
Un4ckn0wl3z <gh0stsync.rt@gmail.com>
2017-02-09 14:45:55 +0700

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,15 @@
+public class  StringCase{
+	public static void main(String[] args) {
+		
+		String text1 =  "ABCD";
+		String text2 =  "abcd";
+		String text3 = "   Hello World!   ";
+		
+		System.out.print(text3.trim());
+		System.out.println(text1.toLowerCase());
+		System.out.print(text3.trim());
+		System.out.println(text2.toUpperCase());
+		System.out.print(text3.trim());
+
+	}
+}
